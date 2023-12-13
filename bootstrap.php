d01a6aff53bd09d68ab8719805b4cb7d9ad5dc68
Gabriel Felix <55044310+bielfelix@users.noreply.github.com>
2023-12-13 03:59:41 -0300

    v1.1

--- bootstrap.php
+++ bootstrap.php
@@ -13,7 +13,7 @@ define(SENHA, 'senha');
 /* OUTRAS CONSTANTES */
 define(DS, DIRECTORY_SEPARATOR);
 define(DIR_APP, __DIR__);
-define(DIR_PROJETO, 'webservice_v2');
+define(DIR_PROJETO, 'api');
 
 if (file_exists('autoload.php')) {
     include 'autoload.php';
